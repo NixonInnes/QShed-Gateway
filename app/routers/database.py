2b2dfae0d0a3206ecb2aaab728ac54801e597d6e
NixonInnes <nixoninnes@echonet.io>
2022-03-15 23:17:42 +0000

    fixed database endpoints and moved response models to client

--- app/routers/database.py
+++ app/routers/database.py
@@ -1,61 +1,32 @@
+from bson.json_util import dumps as json_dumps
 from typing import Union, Dict, List
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter, HTTPException, Request
 
-from qshed.client.models import Request, Schedule
+from qshed.client.models import response as responseModels
+from qshed.client.models.misc import MongoQuery
+
+from .. import mongo_client
+
 
 router = APIRouter()
 
 
-from pydantic import BaseModel
-
-class MongoQuery(BaseModel):
-    key: str
-    query: Union[str, Dict[str,str]]
-
-
-class Response(BaseModel):
-    ok: bool = True
-    message: str = ""
-
-class DictResponse(Response):
-    content: Dict[str,str]
-
-class ListResponse(Response):
-    content: List[str]
-
-class StrResponse(Response):
-    content: str
-
-class IntResponse(Response):
-    content: int
-
-class JSONResponse(StrResponse):
-    pass
-
-
-@router.post("/create", response_model=StrResponse)
+@router.post("/create", response_model=responseModels.StrResponse)
 def create_database(database_name:str):
-    """Create a new database.  
-
-    Returns:  
-        ok (bool): flag whether the request was successful  
-        message (str): status message  
-        content (str): name of the newly created database  
-    """
     try:
         mongo_client[database_name]
     except Exception as e:
         return HTTPException(status_code=500, detail="Unable to create database")
-    return StrResponse(content=database_name, message="database created")
+    return responseModels.StrResponse(content=database_name, message="database created")
 
 
-@router.get("/list", response_model=ListResponse)
+@router.get("/list", response_model=responseModels.ListResponse)
 def list_databases():
     database_names:List[str] = mongo_client.list_database_names()
-    return ListResponse(content=database_names, message="databases created")
+    return responseModels.ListResponse(content=database_names, message="databases created")
 
 
-@router.get("/{database_name}/list", response_model=ListResponse)
+@router.get("/{database_name}/list", response_model=responseModels.ListResponse)
 def list_collections(database_name:str):
     try:
         db = mongo_client[database_name]
@@ -63,27 +34,28 @@ def list_collections(database_name:str):
         return HTTPException(status_code=404, detail="Database not found")
 
     collection_names = db.list_collection_names()
-    return ListResponse(content=collection_names, message=f"{database_name} collections")
+    return responseModels.ListResponse(content=collection_names, message=f"{database_name} collections")
 
 
-@router.get("/{database_name}/{collection_name}/get", response_model=JSONResponse)
-def get_collection(database_name:str, collection_name:str):
+@router.get("/{database_name}/{collection_name}/get", response_model=responseModels.JSONResponse)
+def get_collection(database_name:str, collection_name:str, request:Request):
     return get_limited_collection(database_name, collection_name, 10)
 
 
-@router.get("/{database_name}/{collection_name}/get/{limit}", response_model=JSONResponse)
-def get_limited_collection(database_name:str, collection_name:str, limit:int):
+@router.get("/{database_name}/{collection_name}/get/{limit}", response_model=responseModels.JSONResponse)
+def get_limited_collection(database_name:str, collection_name:str, limit:int, request:Request):
+    query = request.query_params._dict
     try:
         db = mongo_client[database_name]
         col = db[collection_name]
     except:
         return HTTPException(status_code=404, detail="Database/collection not found")
 
-    data = col.find(request.args).sort("$natural", -1).limit(limit)
-    return JSONResponse(content=json.dumps(data), message=f"{database_name}/{collection_name} content (limit={limit})")
+    data = json_dumps(col.find(query).sort("$natural", -1).limit(limit))
+    return responseModels.JSONResponse(content=data, message=f"{database_name}/{collection_name} content (limit={limit})")
 
 
-@router.post("/{database_name}/{collection_name}/insert", response_model=DictResponse)
+@router.post("/{database_name}/{collection_name}/insert", response_model=responseModels.DictResponse)
 def insert_into_collection(database_name:str, collection_name:str, data:dict):
     try:
         db = mongo_client[database_name]
@@ -91,10 +63,10 @@ def insert_into_collection(database_name:str, collection_name:str, data:dict):
     except:
         return HTTPException(status_code=404, detail="Database/collection not found")
     rtn = col.insert_one(data)
-    return StrResponse(content=rtn.inserted_id, message=f"data inserted into {database_name}/{collection_name}")
+    return responseModels.StrResponse(content=rtn.inserted_id, message=f"data inserted into {database_name}/{collection_name}")
 
 
-@router.post("/{database_name}/{collection_name}/insert/many")
+@router.post("/{database_name}/{collection_name}/insert/many", response_model=responseModels.ListResponse)
 def insert_many_into_collection(database_name:str, collection_name:str, data:list):
     try:
         db = mongo_client[database_name]
@@ -103,10 +75,10 @@ def insert_many_into_collection(database_name:str, collection_name:str, data:lis
         return HTTPException(status_code=404, detail="Database/collection not found")
 
     rtn = col.insert_many(data)
-    return ListResponse(content=rtn.inserted_ids, message=f"data inserted into {database_name}/{collection_name}")
+    return responseModels.ListResponse(content=rtn.inserted_ids, message=f"data inserted into {database_name}/{collection_name}")
 
 
-@router.post("/{database_name}/{collection_name}/delete/one", response_model=Response)
+@router.post("/{database_name}/{collection_name}/delete/one", response_model=responseModels.Response)
 def delete_one_from_collection(database_name:str, collection_name:str, query:MongoQuery):
     try:
         db = mongo_client[database_name]
@@ -115,10 +87,10 @@ def delete_one_from_collection(database_name:str, collection_name:str, query:Mon
         return HTTPException(status_code=404, detail="Database/collection not found")
 
     col.delete_one(query)
-    return Response(message=f"1 document deleted from {database_name}/{collection_name}")
+    return responseModels.Response(message=f"1 document deleted from {database_name}/{collection_name}")
 
 
-@router.post("/{database_name}/{collection_name}/delete/many", response_model=IntResponse)
+@router.post("/{database_name}/{collection_name}/delete/many", response_model=responseModels.IntResponse)
 def delete_many_from_collection(database_name:str, collection_name:str, query:MongoQuery):
     try:
         db = mongo_client[database_name]
@@ -127,10 +99,10 @@ def delete_many_from_collection(database_name:str, collection_name:str, query:Mo
         return HTTPException(status_code=404, detail="Database/collection not found")
 
     rtn = col.delete_many(query)
-    return IntResponse(content=rtn.deleted_count, message=f"{rtn.deleted_count} documents deleted from {database_name}/{collection_name}")
+    return responseModels.IntResponse(content=rtn.deleted_count, message=f"{rtn.deleted_count} documents deleted from {database_name}/{collection_name}")
 
 
-@router.post("/{database_name}/{collection_name}/delete/all", response_model=IntResponse)
+@router.post("/{database_name}/{collection_name}/delete/all", response_model=responseModels.IntResponse)
 def delete_all_from_collection(database_name:str, collection_name:str, confirm:bool=False):
     if not confirm:
         return HTTPException(status_code=400, detail="Confirmation option not set")
@@ -142,4 +114,4 @@ def delete_all_from_collection(database_name:str, collection_name:str, confirm:b
         return HTTPException(status_code=404, detail="Database/collection not found")
 
     rtn = col.delete_many({})
-    return IntResponse(content=rtn.deleted_count, message=f"all documents deleted from {database_name}/{collection_name}")
+    return responseModels.IntResponse(content=rtn.deleted_count, message=f"all documents deleted from {database_name}/{collection_name}")
--- app/routers/scheduler.py
+++ app/routers/scheduler.py
@@ -1,19 +1,30 @@
 import httpx
 from fastapi import APIRouter
 
-from qshed.client.models import Schedule
+from qshed.client.models.data import Schedule
+from qshed.client.models.response import ScheduleResponse, SchedulesResponse, StrResponse
 
 router = APIRouter()
 
 SCHEDULER_URL = "http://localhost:5100"
 
 
-@router.put("/add")
+@router.put("/add", response_model=StrResponse)
 async def add_schedule(schedule: Schedule):
-    resp = await httpx.post(SCHEDULER_URL + "/add", data=schedule.json())
-    return resp.content
+    async with httpx.AsyncClient() as client:
+        resp = await client.post(SCHEDULER_URL + "/add", data=schedule.json())
+    return StrResponse(content=resp.content)
 
-@router.get("/list")
+
+@router.get("/get/{job_id}", response_model=ScheduleResponse)
+async def get_schedule(job_id:str):
+    async with httpx.AsyncClient() as client:
+        resp = await client.get(SCHEDULE_URL + f"/get/{job_id}")
+    return ScheduleResponse(content=resp.content)
+
+
+@router.get("/list", response_model=SchedulesResponse)
 async def list_schedules():
-    resp = await httpx.get(SCHEDULER_URL + "/list")
-    return resp.content
+    async with httpx.AsyncClient() as client:
+        resp = await client.get(SCHEDULER_URL + "/list")
+    return SchedulesResponse(**resp.json())
