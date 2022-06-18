4cb8def96eef8c9558fab5dc2e33d18c1eab8db7
NixonInnes <nixoninnes@echonet.io>
2022-06-18 22:19:09 +0100

    added route to add points to ts

--- gateway/app/routers/timeseries.py
+++ gateway/app/routers/timeseries.py
@@ -3,7 +3,7 @@ from typing import List
 from fastapi import APIRouter, HTTPException, Request, Query, Response
 
 from qshed.client.models.data import Timeseries, ts_json_loads
-from qshed.client.models.response import TimeseriesResponse, TimeseriesListResponse
+from qshed.client.models.response import TimeseriesResponse, TimeseriesListResponse, error
 
 from gateway.app import sql_session
 from gateway.app.models import Timeseries as sqlTimeseries
@@ -61,12 +61,12 @@ async def create(timeseries_request: Request):
             status_code=400, 
             detail=f"Timeseries name already exists: {timeseries.name}"
         )
-    ts = sqlTimeseries.create(
+    sql_timeseries = sqlTimeseries.create(
         name=timeseries.name
     )
-    ts.query.add_points(timeseries.data)
+    sql_timeseries.query.add_points(timeseries.data)
     response_model = TimeseriesResponse(
-        data=ts.build_model(
+        data=sql_timeseries.build_model(
             start=timeseries.data.index[0], 
             end=timeseries.data.index[-1]
         )
@@ -74,75 +74,47 @@ async def create(timeseries_request: Request):
     return Response(content=response_model.json(), media_type="application/json")
 
 
-# @router.get("/get/{name}", response_model=responseModels.TimeseriesResponse)
-# def get(name: str, start: str, end: str):
-#     ts_record = sql_session.query(sqlModels.TimeseriesRecord).filter_by(name=name).first()
-#     if ts_record is None:
-#         return responseModels.TimeseriesResponse(
-#             ok=False, 
-#             content="", 
-#             message=f"unable to find timeseries with name: {name}"
-#         )
-#     return get_by_id(ts_record.id, start, end)
+@router.post("/add")
+async def add(timeseries_request: Request):
+    request_body = await timeseries_request.body()
+    timeseries = Timeseries.parse_obj(
+        ts_json_loads(
+            request_body.decode("utf-8")
+        )
+    )
+    if timeseries.id is not None:
+        sql_timeseries = (
+            sql_session
+            .query(sqlTimeseries)
+            .get(timeseries.id)
+        )
+        if sql_timeseries is None:
+            return HTTPException(
+                status_code=404, 
+                detail=f"Invalid Timeseries id: {timeseries.id}"
+            )
+    else:
+        sql_timeseries = (
+            sql_session
+            .query(sqlTimeseries)
+            .filter_by(name=timeseries.name)
+            .first()
+        )
+        if sql_timeseries is None:
+            return HTTPException(
+                status_code=404, 
+                detail=f"Invalid Timeseries name: {timeseries.name}"
+            )
 
+    sql_timeseries.query.add_points(timeseries.data)
+    response_model = TimeseriesResponse(
+        data=sql_timeseries.build_model(
+            start=timeseries.data.index[0], 
+            end=timeseries.data.index[-1]
+        )
+    )
+    return Response(content=response_model.json(), media_type="application/json")
 
-# @router.get("/get/id/{id}", response_model=responseModels.TimeseriesResponse)
-# def get_by_id(id: int, start: str, end: str):
-#     ts_record = sql_session.query(sqlModels.TimeseriesRecord).get(id)
-#     if ts_record is None:
-#         return responseModels.TimeseriesResponse(
-#             ok=False, 
-#             content="", 
-#             message=f"unable to find timeseries with id: {id}"
-#         )
-
-#     if start:
-#         start = datetime.strptime(start, Config.TIME_FORMAT)
-#     if end:
-#         end = datetime.strptime(end, Config.TIME_FORMAT)
-#     content =zip_str(ts_record.get().get_points(start=start, end=end).to_json())
-#     return responseModels.TimeseriesResponse(content=content)
-
-
-# @router.post("/create", response_model=responseModels.TimeseriesRecordResponse)
-# def create(ts_record: dataModels.TimeseriesRecord):
-#     new_record = sqlModels.TimeseriesRecord(name=ts_record.name)
-#     sql_session.add(new_record)
-#     sql_session.commit()
-#     return responseModels.TimeseriesRecordResponse(content=new_record.json())
-
-
-# @router.post("/set/{name}", response_model=responseModels.TimeseriesResponse)
-# def set(name: str, dataFrame: dataModels.DataFrameModel):
-#     ts_record = sql_session.query(
-#         sqlModels.TimeseriesRecord).filter_by(name=name).first()
-#     if ts_record is None:
-#         return responseModels.TimeseriesResponse(
-#             ok=False, 
-#             content="", 
-#             message=f"unable to find timeseries: {name}"
-#         )
-#     return set_by_id(ts_record.id, dataFrame)
-
-
-# @router.post("/set/id/{id}", response_model=responseModels.TimeseriesResponse)
-# def set_by_id(id: int, dataFrame: dataModels.DataFrameModel):
-#     ts_record = sql_session.query(sqlModels.TimeseriesRecord).get(id)
-#     if ts_record is None:
-#         return responseModels.TimeseriesResponse(
-#             ok=False, 
-#             content="", 
-#             message=f"unable to find timeseries with id: {id}"
-#         )
-#     df = dataFrame.parse()
-#     ts_record.get().add_points(df)
-#     return responseModels.TimeseriesResponse(ok=True, content=df.to_json())
-
-
-# @router.get("/list", response_model=responseModels.TimeseriesRecordListResponse)
-# def list():
-#     records = [record.dict() for record in sql_session.query(sqlModels.TimeseriesRecord).all()]
-#     return responseModels.TimeseriesRecordListResponse(content=json.dumps(records))
 
 
 # @router.post("/scan", response_model=responseModels.TimeseriesRecordListResponse)
